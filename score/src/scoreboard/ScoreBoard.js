a9825367801353771f20f04d649cc9f7db2338c2
McNultyPT <mcnultypt@gmail.com>
2019-03-12 10:57:18 -0700

    creating ScoreBoard component to render Dashboard and Display components

---
+++ score/src/scoreboard/ScoreBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ScoreBoard extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ScoreBoard;
